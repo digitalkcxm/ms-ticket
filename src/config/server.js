f91b0d5f51357f0857404c9bb4529c945cb37eed
Rafael Silva <rafaeldsilva16@gmail.com>
2020-10-07 11:48:23 -0300

    include apm info

--- src/config/server.js
+++ src/config/server.js
@@ -5,6 +5,18 @@ const routes = require("./routes.js")
 const cors = require("cors")
 const app = express()
 
+const agent = require('elastic-apm-node').start({
+    serviceName: 'msticket',
+    serverUrl: process.env.APM
+})
+
+const Tracer = require('elastic-apm-node-opentracing')
+
+const tracer = new Tracer(agent)
+
+const span = tracer.startSpan('http_request');
+span.finish();
+
 const server = require("http").createServer(app)
 
 app.use(cors())
