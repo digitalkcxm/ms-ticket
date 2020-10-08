ab0b9215010b33051904794a2ce88d449890a27c
Rafael Silva <rafaeldsilva16@gmail.com>
2020-10-08 18:11:53 -0300

    mongo string param connection

--- src/config/database/mongoConnection.js
+++ src/config/database/mongoConnection.js
@@ -3,7 +3,9 @@ const MongoClient = require('mongodb').MongoClient
 
 module.exports = async (app, callback) => {
 
-    let connectionMongo = process.env.MONGO_STRINGCONNECTION_PARAMS ? `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?${process.env.MONGO_STRINGCONNECTION_PARAMS}` : `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}`
+    let connectionMongo = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+
+    if (process.env.MONGO_STRINGCONNECTION_PARAMS)`${connectionMongo}/${process.env.MONGO_DATABASE}?${process.env.MONGO_STRINGCONNECTION_PARAMS}`
 
     MongoClient.connect(connectionMongo, { useUnifiedTopology: true, promiseLibrary: Promise }, (err, conn) => {
         if (err) console.error(`#00000 - Falha ao conectar ao banco de dados. ${err.stack}`)
