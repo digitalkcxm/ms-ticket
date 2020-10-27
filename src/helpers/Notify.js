c6b6faaf350c5734569fc69987007b1380cd376e
Rafael Silva <rafaeldsilva16@gmail.com>
2020-10-27 17:17:34 -0300

    msnotification env "

--- src/helpers/Notify.js
+++ src/helpers/Notify.js
@@ -2,10 +2,11 @@ const axios = require("axios")
 module.exports = (notify_token, obj) => {
     return axios({
         "method": "post",
-        "url": "https://581e4c7665a7.ngrok.io/api/v1/notification/ticket",
+        "url": process.env.msnotification,
         "headers": {
             "Authorization": notify_token
         },
         "data": obj
     })
 }
+
