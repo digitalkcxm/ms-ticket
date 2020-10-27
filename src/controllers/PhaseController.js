c73e3766ca399036a69e539b3fdb35ac0afe2a38
Rafael Silva <rafaeldsilva16@gmail.com>
2020-10-27 19:54:56 -0300

    add countSLA

--- src/controllers/PhaseController.js
+++ src/controllers/PhaseController.js
@@ -135,9 +135,11 @@ class PhaseController {
                 if (result[i].ticket.length > 0) {
                     for (let ticket of result[i].ticket) {
                         const typeMoment = await new UnitOfTimeModel().checkUnitOfTime(result[i].id_unit_of_time)
-                        // let first_interaction = await ticketModel.first_interaction(id)
                         ticket.countSLA = moment(ticket.created_at).add(result[i].sla_time, typeMoment)
                         ticket.countSLA = moment(ticket.countSLA).format("DD/MM/YYYY HH:mm:ss")
+                        let first_interaction = await ticketModel.first_interaction(ticket.id)
+                        first_interaction ? ticket.first_message = moment(first_interaction).format("DD/MM/YYYY HH:mm:ss") : null
+
                         if (ticket.id_form) {
                             ticket.form_data = await new FormDocuments(req.app.locals.db).findRegister(ticket.id_form)
                             delete ticket.id_form
