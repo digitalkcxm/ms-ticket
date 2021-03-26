0041a043f7d7a14286cd45901814b627fa5e99ae
Rafael <rafaeldsilva16@gmail.com>
2021-03-26 10:24:55 -0300

    fix: return phase department

--- src/models/DepartmentModel.js
+++ src/models/DepartmentModel.js
@@ -4,19 +4,19 @@ class DepartmentModel {
     async getByID(department_id, company_id) {
         try {
             return await database(tableName).select("id")
-            .where("id_company", company_id)
-            .andWhere("id_department_core", department_id)
+                .where("id_company", company_id)
+                .andWhere("id_department_core", department_id)
         } catch (err) {
             console.log("Error when get department by ID => ", err)
             return err
         }
     }
 
-    async create(obj){
-        try{
+    async create(obj) {
+        try {
             return await database(tableName).returning(["id"]).insert(obj)
-        }catch(err){
-            console.log("Error when create department => ",err)
+        } catch (err) {
+            console.log("Error when create department => ", err)
             return err
         }
     }
--- src/models/PhaseModel.js
+++ src/models/PhaseModel.js
@@ -178,6 +178,23 @@ class PhaseModel {
         }
     }
 
+    async getPhasesByDepartmentID(id_department) {
+        try {
+            return await database("department_phase").select({
+                phase: "phase.id",
+                name: "phase.name",
+                form: "phase.form",
+                id_form_template: "phase.id_form_template"
+            })
+                .leftJoin("phase", "phase.id", "department_phase.id_phase")
+                .where("department_phase.id_department", id_department)
+
+        } catch (err) {
+            console.log("Error when catch department id ==>", err)
+            return err
+        }
+    }
+
     async disablePhaseTicket(id_ticket) {
         try {
             return await database('phase_ticket').update({ active: "false" }).where("id_ticket", id_ticket)
