bfa267584862d392ba802622283107a87a1f8ebc
Gabriel Mello <dfmello.gabriel@gmail.com>
2021-11-09 17:14:19 -0300

    fix: rota get

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -10,7 +10,7 @@ const customerController = new CustomerController()
 
 router.get("/:id", (req, res) => customerController.getByID(req, res))
 router.get("/core/:id_core", (req, res) => customerController.getByIDCore(req, res))
-router.get("/ticket/:id_ticket", (req, res) => customerController.getByTicket(req, res))
+router.get("/ticket", (req, res) => customerController.getByTicket(req, res))
 
 
 router.post("/", (req, res) => customerController.create(req, res))
