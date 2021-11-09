35eccc67cc87b68354d8ea7fd885a1629d5a59c9
Gabriel Mello <dfmello.gabriel@gmail.com>
2021-11-09 17:30:53 -0300

    fix: rota update

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -8,12 +8,14 @@ const router = express.Router()
 const companyController = new CompanyController()
 const customerController = new CustomerController()
 
+router.use(verifyCompany);
+
 router.get("/core/:id_core", (req, res) => customerController.getByIDCore(req, res))
 router.get("/ticket", (req, res) => customerController.getByTicket(req, res))
 router.get("/:id", (req, res) => customerController.getByID(req, res))
 
 
 router.post("/", (req, res) => customerController.create(req, res))
-router.put("/", verifyCompany, (req, res) => customerController.update(req, res))
+router.put("/:id", (req, res) => customerController.update(req, res))
 
 module.exports = router
