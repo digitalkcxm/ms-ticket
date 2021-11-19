1ffe60d1b43d31f04b7cc3e1e71e75e2c76aaeac
Rafael Mendes <rafaeldsilva16@gmail.com>
2021-11-19 14:36:13 -0300

    fix: alterando o nome do campo

--- src/config/database/migrations/20211119142801_add_bool_create.js
+++ src/config/database/migrations/20211119142801_add_bool_create.js
@@ -1,13 +1,13 @@
 exports.up = function (knex, Promise) {
     return knex.schema.alterTable("phase", (table) => {
-      table.boolean("create_protocol");
+      table.boolean("create_ticket");
       table.boolean("create_protocol");
     });
   };
   
   exports.down = function (knex, Promise) {
     return knex.schema.alterTable("phase", (table) => {
-      table.dropColumn("create_protocol");
+      table.dropColumn("create_ticket");
       table.dropColumn("create_protocol");
     });
   };
