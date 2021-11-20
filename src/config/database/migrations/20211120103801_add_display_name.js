dbcbb9b3bfdc5283e572eb0e4760d55d309a5910
Rafael Mendes <rafaeldsilva16@gmail.com>
2021-11-20 10:47:00 -0300

    fix: ajustando a migrate do display_name

--- src/config/database/migrations/20211120103801_add_display_name.js
+++ src/config/database/migrations/20211120103801_add_display_name.js
@@ -1,13 +1,11 @@
 exports.up = function (knex, Promise) {
-    return knex.schema.alterTable("ticket", (table) => {
-      table.string("display_name").default(true);
-      
-    });
-  };
-  
-  exports.down = function (knex, Promise) {
-    return knex.schema.alterTable("ticket", (table) => {
-      table.dropColumn("display_name");
-    });
-  };
-  
+  return knex.schema.alterTable("ticket", (table) => {
+    table.string("display_name").default(true);
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.alterTable("ticket", (table) => {
+    table.dropColumn("display_name");
+  });
+};
