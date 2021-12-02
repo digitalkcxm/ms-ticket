6719f3ded9d5d6bf88edcac268c4bad7b91fec13
Rafael Mendes <rafaeldsilva16@gmail.com>
2021-12-02 00:49:16 -0300

    fix: comentando temporariamente as crons

--- src/helpers/CronJob.js
+++ src/helpers/CronJob.js
@@ -5,21 +5,21 @@ const TicketController = require("../controllers/TicketController");
 const ticketController = new TicketController();
 
 // const setTicketAtRedis = new Cron("*/10 * * * *", async () => await ticketController.setTicketAtRedis(), null, true)
-const checkSLA1 = new Cron(
-  "*/15 * * * * *",
-  async () => await ticketController.checkSLA(1),
-  null,
-  true
-);
-const checkSLA2 = new Cron(
-  "*/5 * * * * *",
-  async () => await ticketController.checkSLA(2),
-  null,
-  true
-);
-const checkSLA3 = new Cron(
-  "*/30 * * * * *",
-  async () => await ticketController.checkSLA(3),
-  null,
-  true
-);
+//const checkSLA1 = new Cron(
+//  "*/15 * * * * *",
+//  async () => await ticketController.checkSLA(1),
+//  null,
+//  true
+//);
+//const checkSLA2 = new Cron(
+//  "*/5 * * * * *",
+//  async () => await ticketController.checkSLA(2),
+//  null,
+//  true
+//);
+//const checkSLA3 = new Cron(
+//  "*/30 * * * * *",
+//  async () => await ticketController.checkSLA(3),
+//  null,
+//  true
+//);
