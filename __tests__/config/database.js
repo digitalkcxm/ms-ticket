fa4e27c9a7a1be89b733b2cc9682fc95276cc24d
Willian Johan <johan_willian@hotmail.com>
2022-07-13 17:12:03 -0300

    fix: ajuste em database nas config de teste

--- __tests__/config/database.js
+++ __tests__/config/database.js
@@ -1,34 +1,33 @@
 import dotenv from 'dotenv'
 import knex from 'knex'
 
-import {generate_Companies} from '../utils/generateGenericData.js'
 import knexfile from '../../knexfile.js'
 import logger from '../../src/config/logger.js'
 
-dotenv.config()
-
 logger.info('running pretest script...')
 
+console.log("Configurando Ambiente...")
+dotenv.config()
 const environment = 'testing'
 
+console.log("Configurando Database...")
 const databaseConfig = knexfile[environment]
 databaseConfig.connection.database = 'postgres'
 let database = knex(databaseConfig)
 
-await database.raw(`DROP DATABASE IF EXISTS ${process.env.DB}`)
+await database.raw(`DROP DATABASE IF EXISTS ${process.env.DB_TEST}`)
 await database.destroy()
-console.log('older version of testing database destroyed.')
+console.log('Versão antiga do banco de dados de teste destruída...')
 
 database = knex(databaseConfig)
-await database.raw(`CREATE DATABASE ${process.env.DB} WITH OWNER ${process.env.DB_USERNAME}`)
-console.log('New testing database created.')
+await database.raw(`CREATE DATABASE ${process.env.DB_TEST} WITH OWNER ${process.env.DB_USERNAME_TEST}`)
 await database.destroy()
+console.log('Novo banco de dados de teste criada...')
 
-databaseConfig.connection.database = process.env.DB
+databaseConfig.connection.database = process.env.DB_TEST
 database = knex(databaseConfig)
 database.migrate.latest().then(async function () {
     logger.info('Migrations successfully created.')
-    const result_CompaniesGeneration = await generate_Companies()
-    //await database.destroy() //qst: vai destruir a database? ou os dados?
+    await database.destroy()
     process.exit()
 })
